5957628c650c838de3e4506a5543a3a59df46ffd
Desec <48061253+DATA-GaMi@users.noreply.github.com>
2024-09-14 10:22:30 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace EFlags计算
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.EFlags = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.CFlags = new System.Windows.Forms.Label();
+            this.PFlags = new System.Windows.Forms.Label();
+            this.AFlags = new System.Windows.Forms.Label();
+            this.ZFlags = new System.Windows.Forms.Label();
+            this.SFlags = new System.Windows.Forms.Label();
+            this.TFlags = new System.Windows.Forms.Label();
+            this.IFlags = new System.Windows.Forms.Label();
+            this.DFlags = new System.Windows.Forms.Label();
+            this.OFlags = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EFlags:";
+            // 
+            // EFlags
+            // 
+            this.EFlags.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.EFlags.Location = new System.Drawing.Point(132, 23);
+            this.EFlags.Name = "EFlags";
+            this.EFlags.Size = new System.Drawing.Size(153, 28);
+            this.EFlags.TabIndex = 1;
+            this.EFlags.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.EFlags.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.EFlags_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CF:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(91, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "PF:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(91, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "AF:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(91, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 18);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "ZF:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(91, 263);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 18);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "SF:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(91, 308);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 18);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "TF:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(230, 83);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 18);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "IF:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(230, 128);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 18);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "DF:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(230, 173);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(35, 18);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "OF:";
+            // 
+            // CFlags
+            // 
+            this.CFlags.AutoSize = true;
+            this.CFlags.Location = new System.Drawing.Point(129, 83);
+            this.CFlags.Name = "CFlags";
+            this.CFlags.Size = new System.Drawing.Size(0, 18);
+            this.CFlags.TabIndex = 11;
+            // 
+            // PFlags
+            // 
+            this.PFlags.AutoSize = true;
+            this.PFlags.Location = new System.Drawing.Point(129, 128);
+            this.PFlags.Name = "PFlags";
+            this.PFlags.Size = new System.Drawing.Size(0, 18);
+            this.PFlags.TabIndex = 12;
+            // 
+            // AFlags
+            // 
+            this.AFlags.AutoSize = true;
+            this.AFlags.Location = new System.Drawing.Point(129, 173);
+            this.AFlags.Name = "AFlags";
+            this.AFlags.Size = new System.Drawing.Size(0, 18);
+            this.AFlags.TabIndex = 13;
+            // 
+            // ZFlags
+            // 
+            this.ZFlags.AutoSize = true;
+            this.ZFlags.Location = new System.Drawing.Point(129, 218);
+            this.ZFlags.Name = "ZFlags";
+            this.ZFlags.Size = new System.Drawing.Size(0, 18);
+            this.ZFlags.TabIndex = 14;
+            // 
+            // SFlags
+            // 
+            this.SFlags.AutoSize = true;
+            this.SFlags.Location = new System.Drawing.Point(129, 263);
+            this.SFlags.Name = "SFlags";
+            this.SFlags.Size = new System.Drawing.Size(0, 18);
+            this.SFlags.TabIndex = 15;
+            // 
+            // TFlags
+            // 
+            this.TFlags.AutoSize = true;
+            this.TFlags.Location = new System.Drawing.Point(129, 308);
+            this.TFlags.Name = "TFlags";
+            this.TFlags.Size = new System.Drawing.Size(0, 18);
+            this.TFlags.TabIndex = 16;
+            // 
+            // IFlags
+            // 
+            this.IFlags.AutoSize = true;
+            this.IFlags.Location = new System.Drawing.Point(271, 83);
+            this.IFlags.Name = "IFlags";
+            this.IFlags.Size = new System.Drawing.Size(0, 18);
+            this.IFlags.TabIndex = 17;
+            // 
+            // DFlags
+            // 
+            this.DFlags.AutoSize = true;
+            this.DFlags.Location = new System.Drawing.Point(271, 128);
+            this.DFlags.Name = "DFlags";
+            this.DFlags.Size = new System.Drawing.Size(0, 18);
+            this.DFlags.TabIndex = 18;
+            // 
+            // OFlags
+            // 
+            this.OFlags.AutoSize = true;
+            this.OFlags.Location = new System.Drawing.Point(271, 173);
+            this.OFlags.Name = "OFlags";
+            this.OFlags.Size = new System.Drawing.Size(0, 18);
+            this.OFlags.TabIndex = 19;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(149, 381);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 18);
+            this.label11.TabIndex = 20;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(274, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 34);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(395, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.OFlags);
+            this.Controls.Add(this.DFlags);
+            this.Controls.Add(this.IFlags);
+            this.Controls.Add(this.TFlags);
+            this.Controls.Add(this.SFlags);
+            this.Controls.Add(this.ZFlags);
+            this.Controls.Add(this.AFlags);
+            this.Controls.Add(this.PFlags);
+            this.Controls.Add(this.CFlags);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.EFlags);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EFlags计算器";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox EFlags;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label CFlags;
+        private System.Windows.Forms.Label PFlags;
+        private System.Windows.Forms.Label AFlags;
+        private System.Windows.Forms.Label ZFlags;
+        private System.Windows.Forms.Label SFlags;
+        private System.Windows.Forms.Label TFlags;
+        private System.Windows.Forms.Label IFlags;
+        private System.Windows.Forms.Label DFlags;
+        private System.Windows.Forms.Label OFlags;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EFlags计算
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            var binary = "";
+            try
+            {
+                binary = Convert.ToString(Convert.ToInt32(EFlags.Text, 16), 2);
+            }
+            catch (Exception)
+            {
+            } 
+
+            int Padding = (binary.Length % 4 == 0) ? 0 : 4 - (binary.Length % 4);
+
+            string result = binary.PadLeft(Padding + binary.Length, '0');
+
+            //label11.Text = result.Substring;
+
+            string groupstring = "";
+            for (int i = 0; i < result.Length; i += 4)
+            {
+                groupstring += result.Substring(i, 4) + " ";
+            }
+
+            groupstring = groupstring.Trim();
+
+            //
+
+            try
+            {
+
+                // CF 0
+                if (result[result.Length - 1].ToString() == "1")
+                {
+                    CFlags.ForeColor = Color.Red;
+                    CFlags.Text = result[result.Length - 1].ToString();
+                }
+                else
+                {
+                    CFlags.ForeColor = Color.Black;
+                    CFlags.Text = result[result.Length - 1].ToString();
+                }
+
+                // PF 2
+                if (result[result.Length - 3].ToString() == "1")
+                {
+                    PFlags.ForeColor = Color.Red;
+                    PFlags.Text = result[result.Length - 3].ToString();
+                }
+                else
+                {   
+                    PFlags.ForeColor = Color.Black;
+                    PFlags.Text = result[result.Length - 3].ToString();
+                }
+
+
+                // AF 4
+                if (result[result.Length - 5].ToString() == "1")
+                {
+                    AFlags.ForeColor = Color.Red;
+                    AFlags.Text = result[result.Length - 5].ToString();
+                }
+                else
+                {
+                    AFlags.ForeColor = Color.Black;
+                    AFlags.Text = result[result.Length - 5].ToString();
+                }
+                // ZF 6
+                if (result[result.Length - 7].ToString() == "1")
+                {
+                    ZFlags.ForeColor = Color.Red;
+                    ZFlags.Text = result[result.Length - 7].ToString();
+                }
+                else
+                {
+                    ZFlags.ForeColor = Color.Black;
+                    ZFlags.Text = result[result.Length - 7].ToString();
+                }
+
+
+                // SF 7
+                if (result[result.Length - 8].ToString() == "1")
+                {
+                    SFlags.ForeColor = Color.Red;
+                    SFlags.Text = result[result.Length - 8].ToString();
+                }
+                else
+                {
+                    
+                    SFlags.ForeColor = Color.Black;
+                    SFlags.Text = result[result.Length - 8].ToString();
+                }
+                
+
+                // TF 8
+                if(result[result.Length - 9].ToString() == "1" )
+                {
+                    TFlags.ForeColor = Color.Red;
+                    TFlags.Text = result[result.Length - 9].ToString();
+                }
+                else
+                {
+                    TFlags.ForeColor = Color.Black;
+                    TFlags.Text = result[result.Length - 9].ToString();
+                }
+
+                // IF 9
+                if (result[result.Length - 10].ToString() == "1")
+                {
+                    IFlags.ForeColor = Color.Red;
+                    IFlags.Text = result[result.Length - 10].ToString();
+                }
+                else
+                {
+                    IFlags.ForeColor = Color.Black;
+                    IFlags.Text = result[result.Length - 10].ToString();
+                }
+                // DF 10
+                if (result[result.Length - 11].ToString() == "1")
+                {
+                    DFlags.ForeColor = Color.Red;
+                    DFlags.Text = result[result.Length - 11].ToString();
+                }
+                else
+                {
+                    DFlags.ForeColor = Color.Black;
+                    DFlags.Text = result[result.Length - 11].ToString();
+                }
+                // OF 11
+                if (result[result.Length - 12].ToString() == "1")
+                {
+                    OFlags.ForeColor = Color.Red;
+                    OFlags.Text = result[result.Length - 12].ToString();
+                }
+                else
+                {
+                    OFlags.ForeColor = Color.Black;
+                    OFlags.Text = result[result.Length - 12].ToString();
+                }
+
+                if (CFlags.Text == "1")
+                {
+                    label11.Text = "无符号数溢出";
+                }
+                else if (OFlags.Text == "1")
+                {
+                    label11.Text = "有符号数溢出";
+                }
+                else if (CFlags.Text == "1" && OFlags.Text == "1")
+                {
+                    label11.Text = "出现\n无符号数溢出\n有符号数溢出";
+                }
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+
+        private void EFlags_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // 判断输入的字符是否为数字或控制键（如退格键等）
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                // 阻止非数字的输入
+                e.Handled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CFlags.Text = string.Empty;
+            DFlags.Text = string.Empty;
+            OFlags.Text = string.Empty;
+            AFlags.Text = string.Empty;
+            IFlags.Text = string.Empty;
+            TFlags.Text = string.Empty;
+            PFlags.Text = string.Empty;
+            ZFlags.Text = string.Empty;
+            SFlags.Text = string.Empty;
+            label11.Text = string.Empty;
+        }
+    }
+}
